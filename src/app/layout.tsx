3d384e812810c076b9d14c03b731e20c66155c39
Laurence-R <sdjoid@gmail.com>
2026-01-07 22:04:23 +0800

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
     title: "張英超 | 彰化師大資工系",
     description: "專注於無線網路協定與行動通訊系統的效能優化研究",
+    icons: {
+        icon: "/NCUE_seal.svg",
+    },
 };
 
 export default function RootLayout({
